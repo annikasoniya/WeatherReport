3ab960c8471c0db3ff90f5ceb5d6e648638780ed
annikasoniya <51323126+annikasoniya@users.noreply.github.com>
2019-06-03 15:10:18 +0200

    Adding WeatherConstants.java

--- src/come/weather/util/WeatherConstants.java
+++ src/come/weather/util/WeatherConstants.java
@@ -1,1 +1,14 @@
+package com.weather.util;
 
+public interface WeatherConstants {
+
+	public static String BASE_URL_CURRENT = "http://api.apixu.com/v1/current.json?";
+	public static String BASE_URL_FORECAST = "http://api.apixu.com/v1/forecast.json?";
+	public static String APPID = "69784ec4014b48d2b75152451192205";
+	public static String CONTENT_TYPE_UTF="application/json; charset=UTF-8";
+	public static String ACCEPT_JSON="application/json";
+	public static String GET_METHOD="GET";
+
+	
+
+}
