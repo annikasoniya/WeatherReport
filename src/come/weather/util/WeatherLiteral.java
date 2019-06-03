7faf538bfb04b7fc82f74326caf5e1c8f80309e1
annikasoniya <51323126+annikasoniya@users.noreply.github.com>
2019-06-03 15:10:53 +0200

    adding WeatherLiteral

---
+++ src/come/weather/util/WeatherLiteral.java
@@ -0,0 +1,22 @@
+package com.weather.util;
+
+public interface WeatherLiteral {
+	String CITY="city";
+	String CURRENT="current";
+	String TEMP_C="temp_c";
+	String FORECAST="forecast";
+	String FORECAST_DAY="forecastday";
+	String DAY="day";
+	String MAXTEMP_C="maxtemp_c";
+	String CONDITION ="condition";
+	String ICON="icon";
+	String ERROR="error";
+	String CODE="code";
+	String MESSAGE="message";
+	String ERROR_CODE="ErrorCode";
+	String ERROR_MESSAGE="Errormessage";
+	String CONTENT_TYPE="Content-Type";
+	String ACCEPT="Accept";
+	String TEXT="text";
+
+}
